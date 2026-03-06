cd8a4cc761fe94d1d19e02418a9adc9b6ccfde9c
Sourashis07 <sourashis7ps7sabud@gmail.com>
2026-03-06 15:24:08 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
 /**
- * =====================================================
+ * ============================================================
  * MAIN CLASS - UseCase1PalindromeApp
- * =====================================================
+ * ============================================================
  *
  * Use Case 1: Application Entry & Welcome Message
  *
@@ -19,14 +19,44 @@
  * The goal is to establish a clear startup flow.
  *
  * @author Sourashis
- * @version 1.0
+ * @version 2.0
  */
 
-
-import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version 1.0");
-        System.out.println("System initialized successfully");
-    }}
+
+        System.out.println("WELCOME TO PALINDROME CHECKER APP MANAGEMENT SYSTEM");
+        System.out.println("version:2.0");
+        System.out.println("System instanced successful");
+
+        String Palindrome = "racecar";
+        String NotPalindrome = "notaracecar";
+
+        int i, n, flag;
+
+        flag = 0;
+        n = Palindrome.length();
+        for (i = 0; i < n/2; i++){
+            if(Palindrome.charAt(i) != Palindrome.charAt(n-i-1)) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)  System.out.println("The string " + Palindrome + " is a palindrome.");
+        else System.out.println("The string " + Palindrome + " is not a palindrome.");
+
+
+        flag = 0;
+        n = NotPalindrome.length();
+        for (i = 0; i < n/2; i++){
+            if(NotPalindrome.charAt(i) != NotPalindrome.charAt(n-i-1)) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)  System.out.println("The string " + NotPalindrome + " is a palindrome.");
+        else System.out.println("The string " + NotPalindrome + " is not a palindrome.");
+    }
+}
